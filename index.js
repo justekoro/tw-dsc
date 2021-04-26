4cb92cee920205f4e3776bb47f5c3ee0b576b21c
Mister_KoRo <35098503+korobot@users.noreply.github.com>
2021-04-26 18:15:24 +0200

    :sparkles: Init Twitch

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
+
+// Init Twitch
+
 const t = require('twitch-bot');
 const channels = require('./data/channels.js');
 
 const Twitch = new t({
-    username: process.env.TW_USERNAME,
-    oauth: process.env.OAUTH_TOKEN,
+    username: process.env.TW_USERNAME, // Your twitch username
+    oauth: process.env.OAUTH_TOKEN, // Your twitch oauth token (find it on https://twitchapps.com/tmi)
     channels: channels
 });
+
+Twitch.join(process.env.TW_USERNAME);
+
